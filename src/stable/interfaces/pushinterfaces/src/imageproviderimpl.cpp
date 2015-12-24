562e82adf040306cd76ddc47a7e49283fd8618c3
Victor Arribas <v.arribas.urjc@gmail.com>
2015-12-24 21:32:30 +0100

    fix check typo at imageproviderimpl.cpp

--- src/stable/interfaces/pushinterfaces/src/imageproviderimpl.cpp
+++ src/stable/interfaces/pushinterfaces/src/imageproviderimpl.cpp
@@ -63,8 +63,8 @@ ImageProviderI::getImageData_async (const jderobot::interfaces::AMD_ImageProvide
 void
 ImageProviderI::_getImageData_async (const jderobot::interfaces::AMD_ImageProvider_getImageDataPtr& cb,const std::string& /*format*/, const Ice::Current& /*c*/){
     //Non async response
-    if (!imageData.pixelData.size() == 0)
-        throw DataNotExistException();
+    if (imageData.pixelData.size() == 0)
+        throw DataNotExistException("pixelData is empty");
 
     cb->ice_response(imageData);
 }
